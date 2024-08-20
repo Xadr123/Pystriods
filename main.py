4500b89a71bf36e578deac6da99a25632300b5e7
Xadr123 <hansenxander@gmail.com>
2024-08-19 23:55:22 -0700

    added asteroid splitting

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
             for shot in shots:
                 if asteroid.collide(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         pygame.display.flip()
 
